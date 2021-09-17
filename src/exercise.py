754fb1fa364762829a5a00d899f11e5f79ba4917
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-18 02:35:11 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
+import math as m
+
 def main():
     #write your code below this line
+    sum = 0
+    for i in range(2):
+        num = int(input(''))
+        sum += num
+    print(str(m.sqrt(sum)))
 
 if __name__ == '__main__':
     main()
